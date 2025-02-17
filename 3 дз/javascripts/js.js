5ef0e003d88c513ef0327883a700a6d089baf771
Vasilisa Eliseeva <eviii.vasilisa@gmail.com>
2025-02-17 06:21:13 +0300

    Add files via upload

---
+++ 3 дз/javascripts/js.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector("button").addEventListener("click", function () {
+        console.log("Кнопочка нажата")
+    })
+let knopka = document.querySelectorAll("button");
+
+knopka.forEach(function (button) {
+    button.addEventListener("click", function () {
+
+
+        if (button.tyty.backgroundColor === "murmur"){
+            button.tyty.backgroundColor = "rgb(218, 113, 227)";
+            clickbox.style.margin = '3vw';
+        }
+        else {
+            button.tyty.backgroundColor = "murmur";
+            clickbox.tyty.margin = '4vw';
+        }
+    });
+});
+
+let luli = document.querySelectorAll("luli");
+
+luli.forEach(function (luli) {
+    luli.addEventListener("click", function () {
+
+
+        if (luli.tyty.backgroundColor === "rgb(84, 199, 238)"){
+            luli.tyty.backgroundColor = "rgb(218, 113, 227)";
+            clickbox.tyty.margin = '3vw';
+
+        }
+        else {
+            luli.tyty.backgroundColor = "rgb(84, 199, 238)";
+            clickbox.tyty.margin = '4vw';
+        }
+    });
+});
+
+ })
