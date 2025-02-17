f98852670b06fe95c9e4162e1df2f3e68ffdb2eb
Vasilisa Eliseeva <eviii.vasilisa@gmail.com>
2025-02-17 13:18:46 +0300

    Update js.js

--- 3 дз/javascripts/js.js
+++ 3 дз/javascripts/js.js
@@ -1,40 +1,33 @@
-document.addEventListener("DOMContentLoaded", function () {
-    document.querySelector("button").addEventListener("click", function () {
-        console.log("Кнопочка нажата")
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('button').addEventListener('click', function () {
+    console.log('Кнопочка нажата')
+  })
+
+  let knopka = document.querySelectorAll('button')
+
+  knopka.forEach(function (button) {
+    button.addEventListener('click', function () {
+      if (button.style.backgroundColor === 'rgb(84, 199, 238)') {
+        button.style.backgroundColor = 'rgb(218, 113, 227)'
+        button.style.margin = '3vw'
+      } else {
+        button.style.backgroundColor = 'rgb(84, 199, 238)'
+        button.style.margin = '4vw'
+      }
     })
-let knopka = document.querySelectorAll("button");
+  })
 
-knopka.forEach(function (button) {
-    button.addEventListener("click", function () {
+  // let luli = document.querySelectorAll('.luli')
 
-
-        if (button.tyty.backgroundColor === "murmur"){
-            button.tyty.backgroundColor = "rgb(218, 113, 227)";
-            clickbox.tyty.margin = '3vw';
-        }
-        else {
-            button.tyty.backgroundColor = "murmur";
-            clickbox.tyty.margin = '4vw';
-        }
-    });
-});
-
-let luli = document.querySelectorAll("luli");
-
-luli.forEach(function (luli) {
-    luli.addEventListener("click", function () {
-
-
-        if (luli.tyty.backgroundColor === "rgb(84, 199, 238)"){
-            luli.tyty.backgroundColor = "rgb(218, 113, 227)";
-            clickbox.tyty.margin = '3vw';
-
-        }
-        else {
-            luli.tyty.backgroundColor = "rgb(84, 199, 238)";
-            clickbox.tyty.margin = '4vw';
-        }
-    });
-});
-
- })
+  // luli.forEach(function (luli) {
+  //   luli.addEventListener('click', function () {
+  //     if (luli.style.backgroundColor === 'rgb(84, 199, 238)') {
+  //       luli.style.backgroundColor = 'rgb(218, 113, 227)'
+  //       luli.style.margin = '3vw'
+  //     } else {
+  //       luli.style.backgroundColor = 'rgb(84, 199, 238)'
+  //       luli.style.margin = '4vw'
+  //     }
+  //   })
+  // })
+})
